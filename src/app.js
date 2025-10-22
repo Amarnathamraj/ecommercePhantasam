ef053aa5431b591b98d030d62c82162e50e8225d
Amarnathamraj <amarnath987654@gmail.com>
2025-10-22 10:18:41 +0530

    added error handler middleware for app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const express=require('express')
 const cors=require('cors')
 const morgan=require('morgan')
 const dotenv=require('dotenv')
+const errorHandler=require('./middlewares/errorMiddleware')
 dotenv.config()
 const app=express()
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
-
+app.use(errorHandler)
 module.exports=app;
